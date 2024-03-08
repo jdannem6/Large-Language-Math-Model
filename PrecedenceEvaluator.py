ed6626112e480cf52a2ec1624de81ae81de00103
jdannem6 <jdannem6@kent.edu>
2024-03-08 14:46:12 -0500

    WIP: Added extraction functions for mult, divsion, and exponentiation

--- PrecedenceEvaluator.py
+++ PrecedenceEvaluator.py
@@ -7,6 +7,22 @@
 class PrecedenceEvaluator:
     def __init__(self):
         pass
+    def __extract_exponentiation(self, expression_str):
+        idx = expression_str.find('^')
+        if idx != -1:
+            return self.__extract_operation(expression_str, idx)
+        return ""
+
+    def __extract_mult_divis(self, expression_str):
+        mult_idx = expression_str.find('*')
+        div_idx = expression_str.find('/')
+        if mult_idx == -1 and div_idx == -1:
+            return ""
+        if mult_idx != -1 and (div_idx == -1 or mult_idx < div_idx):
+            return self.__extract_operation(expression_str, mult_idx)
+        if div_idx != -1:
+            return self.__extract_operation(expression_str, div_idx)
+        
     def __extract_add_sub(self, expression_str):
         add_idx = expression_str.find('+')
         sub_idx = expression_str.find('-')
